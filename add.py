676e0656e6190566f1b0c51e57d9e04c3f5c2928
OfirShenkar <ofir3624@gmail.com>
2021-03-10 15:42:52 +0200

    new file

---
+++ add.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 10 15:29:40 2021
+
+@author: אופיר שינקר
+"""
+x=input("eter a first number:")
+y=input("eter a second number:")
+c=int(x)+int(y)
+print(c)
+
